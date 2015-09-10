e5f7cb4a792f9bd2361ef3d3e88db95c79156465
WaynePacker <u13015321@tuks.com>
2015-09-10 15:31:20 +0200

    First Commit

---
+++ app/src/androidTest/java/com/imy320/foultmouth/personaldigitaldairy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.imy320.foultmouth.personaldigitaldairy;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
