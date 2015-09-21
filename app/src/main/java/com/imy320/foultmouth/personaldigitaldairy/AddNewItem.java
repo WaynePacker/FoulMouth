f6712dcd2df1eab7eaa88836aba9b9815a431128
WaynePacker <u13015321@tuks.com>
2015-09-21 13:05:28 +0200

    Minor fixes

--- app/src/main/java/com/imy320/foultmouth/personaldigitaldairy/AddNewItem.java
+++ app/src/main/java/com/imy320/foultmouth/personaldigitaldairy/AddNewItem.java
@@ -6,8 +6,6 @@ import android.app.TimePickerDialog;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.DatePicker;
